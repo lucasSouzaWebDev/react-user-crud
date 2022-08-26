b11cdde5c386807a91e59796cfb3c29d5d65d177
Lucas de Souza <lucassd.k13@gmail.com>
2022-08-26 20:22:06 -0300

    Configured react router.

---
+++ frontend/src/main/Routes.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Routes, Route } from 'react-router-dom'
+
+import Home from '../components/Home/Home'
+import UserCrud from '../components/User/UserCrud'
+
+export default props =>
+    <Routes>
+        <Route exact path='/' element={<Home />} />
+        <Route path='/users' element={<UserCrud />} />
+        <Route path="*" element={<Home />} />
+    </Routes>
