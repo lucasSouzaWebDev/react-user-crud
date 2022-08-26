b963b826e4814fcc5b3249d882c36ff97cada92b
Lucas de Souza <lucassd.k13@gmail.com>
2022-08-25 21:42:38 -0300

    Finished Logo component.

--- frontend/src/components/template/Logo/Logo.jsx
+++ frontend/src/components/template/Logo/Logo.jsx
@@ -1,5 +1,10 @@
 import './Logo.css'
 import React from 'react'
+import logo from '../../../assets/imgs/logo.png'
 
 export default props =>
-    <aside className="logo">Logo</aside>
+    <aside className="logo">
+        <a href="/" className="logo">
+            <img src={logo} alt="logo" />
+        </a>
+    </aside>
